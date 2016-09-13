66bb498c6b88346092fd5c015530080c24f7e1e8
rsampdev <rsampdev@gmail.com>
2016-09-13 18:59:58 -0400

    Chapter ten bronze challenge completed

--- Homepwner/ItemsViewController.m
+++ Homepwner/ItemsViewController.m
@@ -12,6 +12,9 @@
 
 @interface ItemsViewController ()
 
+@property (nullable, nonatomic) NSMutableArray *itemsUnderFifty;
+@property (nullable, nonatomic) NSMutableArray *itemsOverFifty;
+
 @end
 
 @implementation ItemsViewController
@@ -24,21 +27,72 @@
     UIEdgeInsets insets = UIEdgeInsetsMake(statusBarHeight, 0, 0, 0);
     self.tableView.contentInset = insets;
     self.tableView.scrollIndicatorInsets = insets;
+    self.itemsUnderFifty = [[NSMutableArray alloc] init];
+    self.itemsOverFifty = [[NSMutableArray alloc] init];
+    
+    Item *item = nil;
+    
+    for (int i = 0; i < self.itemStore.allItems.count; i++) {
+        item = self.itemStore.allItems[i];
+        
+        if (item.valueInDollars >= 50) {
+            [self.itemsOverFifty addObject:item];
+        } else if (item.valueInDollars < 50) {
+            [self.itemsUnderFifty addObject:item];
+        }
+    }
 }
 
 // MARK: - Table View Data Source and Delegate
 
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    if (self.itemsUnderFifty != nil && self.itemsOverFifty != nil) {
+        if (self.itemsUnderFifty.count >= 1 && self.itemsOverFifty.count >= 1) {
+            return 2;
+        } else if (self.itemsUnderFifty.count >= 1) {
+            return 1;
+        } else {
+            return 0;
+        }
+    } else {
+        return 0;
+    }
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    if (section == 0) {
+        return @"Items under $50";
+    } else if (section == 1) {
+        return @"Items over $50";
+    } else {
+        return @"Unknown";
+    }
+}
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.itemStore.allItems.count;
+    if (section == 0) {
+        return self.itemsUnderFifty.count;
+    } else if (section == 1) {
+        return self.itemsOverFifty.count;
+    } else {
+        return 0;
+    }
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
     
-    Item *item = self.itemStore.allItems[indexPath.row];
+    Item *item = nil;
+    NSInteger section = indexPath.section;
+    NSInteger row = indexPath.row;
+
+    if (section == 0 && row < self.itemsUnderFifty.count) {
+        item = self.itemsUnderFifty[row];
+    } else if (section == 1 && row < self.itemsOverFifty.count) {
+        item = self.itemsOverFifty[row];
+    }
     
     cell.textLabel.text = item.name;
-    
     cell.detailTextLabel.text = [NSString stringWithFormat:@"$%d", item.valueInDollars];
     
     return cell;
